19d5aa77278037a62d318a5d2ed286214db2a821
jhusak <q_h@o2.pl>
2026-06-06 20:41:48 +0200

    Update comment for renaming action.lst file

--- tools/make_tab_from_lst.sh
+++ tools/make_tab_from_lst.sh
@@ -1,6 +1,6 @@
 # .lst file obtaining:
 # Go to JAC!'s ACTION! repository
 # compile: mads ACTION-ROM-OSS-16k.asm -l
-# rename it to action.lst
+# rename ACTION-ROM-OSS-16k.lst to action.lst
 #
 grep -w "[\.]proc" action.lst | gawk '{if ($3!=".proc") { if  (strtonum("0x" $2) <0xA000) next; printf "functab[0x%s]=\"%s",$2,$3; for(i=5; i<=NF; i++) printf " %s", $i; print "\";";}}'
