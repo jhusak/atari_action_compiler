7e000e26749353c56ca546e178db397005a44b25
Jakub Husak <jakub.husak@ffserwis.pl>
2026-06-16 23:25:19 +0200

    added test

---
+++ tests/teststr.sh
@@ -0,0 +1,17 @@
+t=${0%.sh}
+X=${t}.xex
+T=${t^^}
+TC=$T.COM
+TL=$TC.lib
+
+rm -f $X
+cd ..
+make || exit
+cd -
+
+../actionc $T.ACT $TC -l  -C -a -w &&
+cat $TL $TC >$X &&
+atari800 -xl-rev altirra -cart "" $X &&
+atari800 -xl-rev 11 -cart "" $X &&
+rm -f $X $TL $TC ||
+echo ERROR
